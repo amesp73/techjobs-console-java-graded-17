8dde8a8b5103a957f9fd7cf10e33e6daa288d3f7
amesp73 <amitrpatel502@gmail.com>
2023-09-05 20:37:27 -0500

    fixed print jobs no result error to be case insensitive

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -75,7 +75,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -95,7 +95,6 @@ public class JobData {
         loadData();
         // TODO - implement this method
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
-
         for(HashMap<String, String> row : allJobs) {
             if(row.toString().toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
